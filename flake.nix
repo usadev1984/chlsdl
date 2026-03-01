4771eb7a8feed845eee63c787487f57834d4ea51
usadev1984 <chadodev84@proton.me>
2026-03-01 20:30:43 +0300

    add flake.nix
    Signed-off-by: usadev1984 <chadodev84@proton.me>

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      # nixpkgs-unstable,
+      utils,
+      ...
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        # pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.clang-tools
+            pkgs.clang
+            pkgs.bear
+            pkgs.pkg-config
+            pkgs.gcc
+          ];
+          buildInputs = with pkgs; [
+            xorg.libX11.dev
+            libxmu.dev
+          ];
+          hardeningDisable = [ "all" ];
+        };
+        packages.default = pkgs.stdenv.mkDerivation rec {
+          pname = "chlsdl";
+          version = "0.0.1";
+
+          src = ./.;
+
+          buildInputs = with pkgs; [
+            xorg.libX11.dev
+            libxmu.dev
+          ];
+
+          buildPhase = ''
+            make -B libchlsdl-common COLOR=1
+            make -B release COLOR=1
+          '';
+          installPhase = ''
+            make install PREFIX=$out
+          '';
+          hardeningDisable = [ "all" ];
+        };
+      }
+    );
+}
