03aef99240ee2d3938b4d5e5e6db0fa53bf467a5
usadev1984 <chadodev84@proton.me>
2026-03-02 03:43:56 +0300

    add deallocator for curl_buffer struct
    Signed-off-by: usadev1984 <chadodev84@proton.me>

--- include/chlsdl/common/util/curl_request.h
+++ include/chlsdl/common/util/curl_request.h
@@ -1,6 +1,8 @@
 #ifndef CURL_REQUEST_H_
 #define CURL_REQUEST_H_
 
+#include <chlsdl/macros.h>
+#include <stdlib.h>
 #include <string.h>
 
 struct curl_buffer {
@@ -11,5 +13,17 @@ struct curl_buffer {
 
 extern struct curl_buffer *
 curl_buffer_alloc(size_t n);
+
+CHLSDL_ALWAYS_INLINE void
+curl_buffer_dealloc(struct curl_buffer * buf)
+{
+    if (!buf)
+        return;
+
+    if (buf->data)
+        free(buf->data);
+
+    free(buf);
+}
 
 #endif // CURL_REQUEST_H_
